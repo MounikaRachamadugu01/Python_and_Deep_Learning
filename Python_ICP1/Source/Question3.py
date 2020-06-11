4011864d5ad180a2ccc6ae543c81f5946ea433ed
MounikaRachamadugu01 <mrnxq@mail.umkc.edu>
2020-06-10 19:23:16 -0500

    Create Question3.py

---
+++ Python_ICP1/Source/Question3.py
@@ -0,0 +1,6 @@
+# Program that accepts a sentence and replace each occurrence of ‘python’ with ‘pythons’ without using regex
+
+# Taking input string
+str = input("Enter your input string:")
+# Replacing the word in the string and displaying
+print("After replacing the word, the input string is changed as ", str.replace("python", "Pythons"))
