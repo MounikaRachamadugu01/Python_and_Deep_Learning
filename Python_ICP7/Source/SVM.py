efc0b0cd96486049487da5a5de4cbc77e83d2a99
MounikaRachamadugu01 <mrnxq@mail.umkc.edu>
2020-06-30 22:22:54 -0500

    Update SVM.py

--- Python_ICP7/Source/SVM.py
+++ Python_ICP7/Source/SVM.py
@@ -31,7 +31,7 @@ predict = svm_clf.predict(X_test_tfidf)
 score = metrics.accuracy_score(twenty_test.target, predict)
 print("\n Score with SVM Classifier: ", score*100)
 
-# Changine the tfidf vectorizer
+# Changing the tfidf vectorizer
 twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
 tfidf_Vect = TfidfVectorizer(ngram_range=(1, 2))
 X_train_tfidf = tfidf_Vect.fit_transform(twenty_train.data)
@@ -51,8 +51,8 @@ nb_clf = MultinomialNB()
 nb_clf.fit(X_train_tfidf, twenty_train.target)
 twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
 X_test_tfidf = tfidf_Vect.transform(twenty_test.data)
-predicted = nb_clf.predict(X_test_tfidf)
-score = metrics.accuracy_score(twenty_test.target, predicted)
+predict = nb_clf.predict(X_test_tfidf)
+score = metrics.accuracy_score(twenty_test.target, predict)
 print("\n Score after setting an argument stop_words = 'english' : ", score*100)
 
 
